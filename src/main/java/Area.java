571e3a7b11fef5e1fff4a1e25778f4842750ddff
Валентин <valmak04@gmail.com>
2021-10-14 15:34:44 +0400

    0.3

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -1,3 +1,16 @@
 public class Area {
+    public static void main(String[] args) {
+    }
 
+    public static double getArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double getArea(int width, int length) {
+        return width*length;
+    }
+
+    public static double getArea(double radius, double height) {
+        return 2 * Math.PI * radius * (height + radius);
+    }
 }
